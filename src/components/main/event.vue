fdbc26a4496e992bb930029d02c73085f46f6cb8
HYUNJI PYUN <pyunjiology@gmail.com>
2021-11-22 11:05:25 +0900

    메인 화면 이벤트/신상품/베스트 영역 구분

---
+++ src/components/main/event.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+        <v-img
+          :src="require('@/assets/photos/event.png')"
+          class="my-3"
+          contain
+          fluid
+        />
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Event',
+
+    data: () => ({
+     
+    }),
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <hello-world />
+  <div>
+    <v-col>
+      <v-row rows="12">
+      <event />
+      </v-row>
+      <v-row rows="12">
+      <new />
+      </v-row>
+      <v-row rows="6">
+      <best />
+      </v-row>
+    </v-col>
+  </div>
+  
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import Event from "@/components/main/event";
+import New from "@/components/main/new"
+import Best from "@/components/main/best"
 
-  export default {
-    name: 'Home',
+export default {
+  name: "Home",
 
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    Event,
+    New,
+    Best
+  },
+};
 </script>
