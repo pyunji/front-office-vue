e7e83e0da5ddc3c7ae0371c6a107cfb4924f73fb
HYUNJI PYUN <pyunjiology@gmail.com>
2021-11-22 11:05:54 +0900

    앱 바 수정 / 탭 추가 / 푸터 홈 아이콘 클릭시 홈으로 이동

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -1,21 +1,35 @@
 <template>
-  <div>
-    <v-app-bar color="black" dark app>
-      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
-      <v-row align="center">
-        <v-img src="@/assets/logo.png" width="15"></v-img>
-        <v-icon>mdi-cart-outline</v-icon>
-      </v-row>
-    </v-app-bar>
-    <v-app-bar color="black" dark app>
-      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
-      <v-row align="center">
-        <v-img src="@/assets/logo.png" width="15"></v-img>
-        <v-icon>mdi-cart-outline</v-icon>
-      </v-row>
-    </v-app-bar>
+  <v-card>
+    <v-toolbar color="transparent" flat>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>| THE | HANDSOME |</v-toolbar-title>
 
-  </div>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-cart-outline</v-icon>
+      </v-btn>
+      <template v-slot:extension>
+        <v-tabs v-model="tab" align-with-title>
+          <v-tabs-slider color="primary"></v-tabs-slider>
+
+          <v-tab v-for="item in items" :key="item">
+            <router-link :to="item.link" v-text="item.tab">
+            {{ item.tab }}
+            </router-link>
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="item in items" :key="item">
+        <v-card flat>
+          <v-card-text v-text="text"></v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
 </template>
 
 <script>
@@ -24,6 +38,11 @@ export default {
   data() {
     return {
       drawer: false,
+      items: [
+        {tab: '홈', link: '/'},
+        {tab: '신상품', link: 'new'},
+        {tab: '베스트', link: 'best'},
+      ],
     };
   },
 };
