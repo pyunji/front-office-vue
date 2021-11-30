343716c7f7cebe689e9807e872027740d30be382
HYUNJI PYUN <pyunjiology@gmail.com>
2021-11-30 14:17:20 +0900

    로그인이 필요한 서비스 interceptor 설정(수정)

--- src/apis/member/cart.js
+++ src/apis/member/cart.js
@@ -4,6 +4,14 @@ const instance = axios.create({
   baseURL: 'http://localhost:81'
 });
 
+/* 로그인이 필요한 서비스에서 session의 token을 가져와 초기화하는 설정 시작 */
+instance.interceptors.request.use(function (config) {
+  const token = sessionStorage.getItem('authToken');
+  config.headers.Authorization =  token ? `Bearer ${token}` : '';
+  return config;
+});
+/* 로그인이 필요한 서비스에서 session의 token을 가져와 초기화하는 설정 시작 */
+
 function cartItems() {
   return instance.get("/cart");
 }
