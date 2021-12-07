14adc73a6bf61fe03edddcc7d6061f219146b735
eunsol <a2670624@naver.com>
2021-12-07 16:19:37 +0900

    홈 화면 상품 리스트 store 적용

--- src/components/main/new.vue
+++ src/components/main/new.vue
@@ -60,24 +60,47 @@ export default {
     showDetail(pcolorId) {
       this.$router.push(`/product/productDetail?pcolorId=${pcolorId}`);
     },
-    changeItems(d1name) {
-      main.getNewItems(d1name).then(response => {
-      this.items = response.data;
-    });
-    }
+    async changeItems(d1name) {
+      await main.getNewItems(d1name)
+        .then(response=>{
+          this.$store.commit('homeStore/setNewItems',response.data);
+        })
+        .catch(error=>{
+          console.log(error);
+        })
+    },
   },
 
-  beforeCreate() {
-    main.getD1Names()
-    .then(response => {
-      this.d1names = response.data;
-    });
-    main.getNewItems().then(response => {
-      this.items = response.data;
-    });
-  
+  beforeMount() {
+    // main.getD1Names()
+    // .then(response => {
+    //   this.d1names = response.data;
+    // });
+    this.d1names = this.$store.getters["homeStore/getD1Names"];
+    // main.getNewItems().then(response => {
+    //   this.items = response.data;
+    // });
+    this.items = this.$store.getters["homeStore/getNewItems"];  
   },
-
-
+  computed:{
+    d1Names(){
+      // store의 d1Names이 변경되었는지 감시
+      return this.$store.getters["homeStore/getD1Names"];
+    },
+    newItems(){
+      // store의 newItems 변경되었는지 감시
+      return this.$store.getters["homeStore/getNewItems"];
+    },
+  },
+  // computed 메서드와 watch 메서드의 이름이 일치하면 store의 데이터를 감시할 수 있다.
+  watch:{
+    // computed의 메서드와 watch의 메서드 명이 일치해야한다.
+    d1Names(to,from){
+      this.d1names = to;
+    },
+    newItems(to,from){
+      this.items = to;
+    },
+  }
 };
 </script>
