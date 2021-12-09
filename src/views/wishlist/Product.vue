7a7fce40e6a2fd64d1a62c2f129a536f9925fd34
eunsol <a2670624@naver.com>
2021-12-10 04:23:29 +0900

    위시리스트 하트아이콘

--- src/views/wishlist/Product.vue
+++ src/views/wishlist/Product.vue
@@ -1,16 +1,22 @@
 <!--컴포넌트 UI 정의-->
 <template>
   <div>
-    <section class="py-5">
-      <div class="container px-4 px-lg-5 mt-5">
-        <div class="row gx-4 gx-lg-5 row-cols-md-3 row-cols-xl-4 justify-content-center">
-          <div v-for="(item, i) in wishItems" :key="i">
-            <v-card class="mx-auto mr-2 mb-2" width="170" @click="showDetail(item.pcolorid)">
+    <section>
+      <div class="container mt-2">
+        <div class="row row-cols-md-3 row-cols-xl-4 justify-content-center">
+          <div v-for="(item, i) in wishItems" :key="i" class="mb-4">
+            <v-card class="h-100 mx-auto" width="180" @click="showDetail(item.pcolorid)">
               <!-- Product image-->
               <div class="text-center">
                 <div class="mb-1" >
+                  <div class="wish-icon-div">
+                      <v-btn icon @click="gohome()">
+                        <v-icon color="red">mdi-heart</v-icon>
+                      </v-btn>
+                  </div>
                       <img :src="`${item.img1}`" width="100%"/>
                 </div>
+
                 <!-- Product details-->
                 <v-card-text class="card-body p-2">
                   <div class="text-center">
@@ -50,6 +56,9 @@ export default {
   },
   //컴포넌트 메소드 정의
   methods:{
+    gohome() {
+      this.$router.push("/");
+    },
     showDetail(pcolorid) {
       this.$router.push(`/product/productDetail?pcolorId=${pcolorid}`);
     },
@@ -67,5 +76,22 @@ export default {
 <!--컴포넌트 스타일 정의-->
 <!--scoped를 생략하면 전역으로 사용-->
 <style scoped>
+    .wish-icon-div {
+      position: absolute;
+      z-index: 2;
+      color: whitesmoke;
+      font-size: 20px;
+      text-align: right;
+      cursor: pointer;
+      border-radius: 10rem;
+      background-color: transparent;
+      border: transparent;
+      width: 99%;
+      padding-top: 4px;
+      padding-right: 4px;
+  }
 
+  .wish-icon-div :hover {
+      color: red;
+  }
 </style>
